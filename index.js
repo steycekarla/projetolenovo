e0cb359d4bcb1d2368131a6e794d30851fe1a606
steycekarla <steycekarla1@gmail.com>
2026-07-01 18:00:08 +0000

    atividade

---
+++ index.js
@@ -0,0 +1,9 @@
+const botoes = document.querySelectorAll("button");
+botoes.forEach(botao => {
+    botao.addEventListener("click", function () {
+
+    
+        window.location.href = "indisponivel.html";
+
+    });
+});
